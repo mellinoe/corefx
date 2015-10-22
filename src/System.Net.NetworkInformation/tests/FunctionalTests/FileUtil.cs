45761049aaefb6f90e26ab4951a593824ecf6117
Eric Mellino <erme@microsoft.com>
2015-10-22 16:33:15 -0700

    Separate out Unix string parsing logic and test it
    These changes separate out all of the string parsing logic into a
    separate class, which I have added unit tests for. The unit tests
    operate on some static test files that I have created / copied from my
    dev machine. They are mostly unchanged, but I have added some more
    interesting data to the files in some cases.
    
    This should protect us from breaks in the parsing logic in the future,
    as we are using quite a bit of shared, common code that might be changed
    often in the future. The only issue that I encountered during the
    writing of these tests was that the Linux parsing logic for current
    active/listening TCP and UDP sockets was using the wrong entry in the
    table. It was using the remote endpoint rather than the local endpoint
    as listed in the file.
    
    Separate out Unix string parsing logic and test it
    
    Move ICMPv6, IPv4, IPv6 statistics parsing to helper class
    
    Separate all of the global statistics parsing for Linux
    
    Separate IPInterfaceStatistics parsing for Linux
    
    Separate out remainder of LinuxNetworkInterface string parsing
    
    Separate TCP/UDP connection parsing
    
    Separate string parsing in LinuxIpv4/6InterfaceProperties
    
    Add missing files to test project
    
    Separate out string parsing from LinuxIPInterfaceProperties
    
    Finish separating out the rest of the string parsing functions.
    
    Cleaned up some naming things and project files
    
    Add tests for all protocol statistics and connection infos
    
    Finish up adding string parsing tests
    
    Write out normalized files on Unix
    
    Fix test file paths, and fix a missed Trim() call
    
    Fix issue with Tcp6 statistics parsing (wrong file)

---
+++ src/System.Net.NetworkInformation/tests/FunctionalTests/FileUtil.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace System.Net.NetworkInformation.Tests
+{
+    internal static class FileUtil
+    {
+        public static void NormalizeLineEndings(string source, string normalizedDest)
+        {
+            // I'm storing the test text assets with their original line endings ('\n').
+            // The parsing logic depends on Environment.NewLine, so we normalize beforehand.
+            string contents = File.ReadAllText(source);
+            if (!contents.Contains(Environment.NewLine))
+            {
+                string otherEnding = Environment.NewLine == "\n" ? "\r\n" : "\n";
+                contents = contents.Replace(otherEnding, Environment.NewLine);
+            }
+
+            File.WriteAllText(normalizedDest, contents);
+        }
+    }
+}
