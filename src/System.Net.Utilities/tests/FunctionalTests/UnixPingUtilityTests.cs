399b5629f01fba334d94a2fdc2317453ed7c0a00
Eric Mellino <erme@microsoft.com>
2015-11-20 14:17:16 -0800

    Add some validation/sanity tests for the Unix ping command line utility

---
+++ src/System.Net.Utilities/tests/FunctionalTests/UnixPingUtilityTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Net.Utilities.Tests;
+
+using Xunit;
+
+namespace System.Net.NetworkInformation.Tests
+{
+    // Contains a few basic validation tests to ensure that the local machine's ping utility
+    // supports the types of options we need to use and formats its output in the way
+    // that we expect it to in order to provide un-privileged Ping support on Unix.
+    public class UnixPingUtilityTests
+    {
+        private const int IcmpHeaderLengthInBytes = 8;
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(50)]
+        [InlineData(1000)]
+        [PlatformSpecific(PlatformID.AnyUnix)]
+        public static void PacketSizeIsRespected(int payloadSize)
+        {
+            IPAddress localAddress = TestSettings.GetLocalIPAddress().Result;
+            string arguments = UnixCommandLinePing.ConstructCommandLine(payloadSize, localAddress.ToString());
+            string utilityPath = (localAddress.AddressFamily == Sockets.AddressFamily.InterNetwork)
+                ? UnixCommandLinePing.Ping4UtilityPath
+                : UnixCommandLinePing.Ping6UtilityPath;
+
+
+            ProcessStartInfo psi = new ProcessStartInfo(utilityPath, arguments);
+            psi.RedirectStandardError = true;
+            psi.RedirectStandardOutput = true;
+            Process p = Process.Start(psi);
+            p.WaitForExit(TestSettings.PingTimeout);
+            string pingOutput = p.StandardOutput.ReadToEnd();
+
+            // Validate that the returned data size is correct.
+            // It should be equal to the bytes we sent plus the size of the ICMP header.
+            int receivedBytes = ParseReturnedPacketSize(pingOutput);
+            Assert.Equal(payloadSize + IcmpHeaderLengthInBytes, receivedBytes);
+
+            // Validate that we only sent one ping with the "-c 1" argument.
+            int numPingsSent = ParseNumPingsSent(pingOutput);
+            Assert.Equal(1, numPingsSent);
+        }
+
+        private static int ParseReturnedPacketSize(string pingOutput)
+        {
+            int indexOfBytesFrom = pingOutput.IndexOf("bytes from");
+            int previousNewLine = pingOutput.LastIndexOf(Environment.NewLine, indexOfBytesFrom);
+            string number = pingOutput.Substring(previousNewLine + 1, indexOfBytesFrom - previousNewLine);
+            int parsedReceivedBytes;
+            if (!int.TryParse(number, out parsedReceivedBytes))
+            {
+                throw new InvalidOperationException("Couldn't parse the ping utility's output.");
+            }
+
+            return parsedReceivedBytes;
+        }
+
+        private static int ParseNumPingsSent(string pingOutput)
+        {
+            int indexOfPacketsTransmitted = pingOutput.IndexOf("packets transmitted");
+            int previousNewLine = pingOutput.LastIndexOf(Environment.NewLine, indexOfPacketsTransmitted);
+            string number = pingOutput.Substring(previousNewLine + 1, indexOfPacketsTransmitted - previousNewLine);
+
+            int parsedNumPings;
+            if (!int.TryParse(number, out parsedNumPings))
+            {
+                throw new InvalidOperationException("Couldn't parse the ping utility's output.");
+            }
+
+            return parsedNumPings;
+        }
+    }
+}
