6cd554528f7b4aaa38f4705513c27e5986c8bce4
Eric Mellino <erme@microsoft.com>
2017-06-30 15:35:37 -0700

    Replace mono's Drawing2D code with ours.

--- src/System.Drawing.Common/src/mono/System.Drawing/Region.cs
+++ src/System.Drawing.Common/src/mono/System.Drawing/Region.cs
@@ -556,8 +556,7 @@ namespace System.Drawing
             status = GDIPlus.GdipGetRegionData(nativeRegion, buff, size, out filled);
             GDIPlus.CheckStatus(status);
 
-            RegionData rgndata = new RegionData();
-            rgndata.Data = buff;
+            RegionData rgndata = new RegionData(buff);
 
             return rgndata;
         }
